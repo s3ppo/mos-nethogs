09ed7b683cffb52b85b01b8c1b818290943d84b5
S3ppo <harald@moniholz.at>
2026-04-04 14:32:30 +0200

    feat: add icon URL to plugin configuration for improved visual representation

--- page/plugin.config.js
+++ page/plugin.config.js
@@ -3,7 +3,7 @@ export default {
   displayName: 'Htop for MOS',
   description: 'Monitor system resources with Htop in MOS',
   version: '0.0.1',
-  icon: '',
+  icon: 'https://raw.githubusercontent.com/s3ppo/mos-htop/4d30e45d1b89efabf708b59b0966d81bffe2ae05/page/assets/pulse.png',
   author: 's3ppo',
   homepage: 'https://github.com/s3ppo/mos-htop',
 };
